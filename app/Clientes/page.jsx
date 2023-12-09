0cb0f675e2c6a4f46fa5f36c9cfcc7a2b0f885ed
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-12-08 21:23:19 -0300

    quedate quieto

--- app/Clientes/page.jsx
+++ app/Clientes/page.jsx
@@ -116,26 +116,26 @@ const clientes = () => {
               />
             </div>
             {esVip && (
-  <div className="block text-sm font-medium text-gray-600">
-    <label
-      htmlFor="clientDate"
-      className="block text-sm font-medium text-gray-600"
-    >
-      Fecha de Ingreso
-    </label>
-    <div className="mt-2">
-      <input
-        type="date"
-        id="clientDate"
-        name="clientDate"
-        value={clientDate}
-        onChange={handleClientDateChange}
-        autoComplete="address-level1"
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-      />
-    </div>
-  </div>
-)}
+              <div className="block text-sm font-medium text-gray-600">
+                <label
+                  htmlFor="clientDate"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Fecha de Ingreso
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="date"
+                    id="clientDate"
+                    name="clientDate"
+                    value={clientDate}
+                    onChange={handleClientDateChange}
+                    autoComplete="address-level1"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className="mt-6 flex items-center justify-end gap-x-6">
