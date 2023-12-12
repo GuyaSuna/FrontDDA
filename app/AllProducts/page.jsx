11cbe8fdf782e488af431b224b66e7229caf7565
AleSpinelli2 <alejospinelli2@gmail.com>
2023-12-12 11:35:54 -0300

    Actualizado rama alejo

--- app/AllProducts/page.jsx
+++ app/AllProducts/page.jsx
@@ -72,12 +72,12 @@ const allProducts = () => {
                   {product.description}
                 </p>
               </div>
-              <div className="p-6 pt-0">
+              <div className="p-6 pt-0 " >
                 <button
-                  className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  className="block w-full select-none rounded-lg text-gray-700 shadow-md bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                   type="button"
                 >
-                  Add to Cart
+                  Ver detalle
                 </button>
               </div>
             </div>
