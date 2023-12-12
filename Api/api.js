6a36b9c4548cab862d5ee21a02a14f784be192d5
AlexSumner <111889133+AlexSumner@users.noreply.github.com>
2023-12-12 13:41:08 -0300

    AltaProducto
    Productos

--- Api/api.js
+++ Api/api.js
@@ -63,6 +63,47 @@ const VentaRegister = async (
   }
 };
 
+const ProductRegister = async (
+  codProd,
+  nombre,
+  descripcion,
+  precio,
+  cantStock,
+  imageUrl
+) => {
+  try {
+    const response = await fetch(`${URL}products`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        codProd,
+        nombre,
+        descripcion,
+        precio,
+        cantStock,
+        imageUrl,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+
+    if (data.codProd) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(`An error has ocurred in Producto: ${error.message}`);
+    return false;
+  }
+};
+
 const ClientRegister = async (name, address, phone, date) => {
   try {
     if (date != "") {
@@ -126,4 +167,10 @@ const getAllProducts = async () => {
   }
 };
 
-export { logIn, getAllProducts, ClientRegister, VentaRegister };
+export {
+  logIn,
+  getAllProducts,
+  ClientRegister,
+  VentaRegister,
+  ProductRegister,
+};
--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -92,7 +92,7 @@ const Sidebar = () => {
         </li>
         <li>
           <Link
-            href="/AllProducts"
+            href="/Productos"
             className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6"
           >
             <span className="inline-flex justify-center items-center ml-4">
@@ -111,7 +111,9 @@ const Sidebar = () => {
                 />
               </svg>
             </span>
-            <span className="ml-2 text-sm tracking-wide truncate">Alta Productos</span>
+            <span className="ml-2 text-sm tracking-wide truncate">
+              Alta Productos
+            </span>
           </Link>
         </li>
         <li>
@@ -161,9 +163,7 @@ const Sidebar = () => {
                 />
               </svg>
             </span>
-            <span className="ml-2 text-sm tracking-wide truncate">
-              Venta
-            </span>
+            <span className="ml-2 text-sm tracking-wide truncate">Venta</span>
           </Link>
         </li>
       </ul>
