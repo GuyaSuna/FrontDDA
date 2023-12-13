1023f269084973921c75d7089b2d827007c3d301
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-12-13 18:12:54 -0300

    automatizacion de precios venta

--- app/Venta/page.jsx
+++ app/Venta/page.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import { VentaRegister, getAllProducts } from "../../Api/api";
-import { useUsuario } from '../../Context/userContext'
+import { useUsuario } from "../../Context/userContext";
 
 const Venta = () => {
   const { usuario } = useUsuario();
@@ -13,7 +13,7 @@ const Venta = () => {
   const [purchaseDate, setPurchaseDate] = useState("");
   const [productList, setProductList] = useState([]);
   const [productsChecked, setProductsChecked] = useState([]);
-  const [totalSale, setTotalSale] = useState("");
+  const [totalSale, setTotalSale] = useState(0);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const openModal = () => {
@@ -25,7 +25,28 @@ const Venta = () => {
   };
 
   const handleProductSelect = (product) => {
-    setProductsChecked([...productsChecked, product]);
+    setProductsChecked((prevProducts) => [
+      ...prevProducts,
+      { ...product, cantidad: 1 },
+    ]);
+    console.log(product)
+    calculateTotalSaleAdd(product.precio);
+  };
+  const calculateTotalSaleAdd = (precio) => {
+    setTotalSale(totalSale + precio);
+  };
+
+  const calculateTotalSaleRemove = (precio) => {
+    setTotalSale(totalSale - precio);
+  };
+
+  const handleDeleteProductSelect = (index , precio) => {
+    setProductsChecked((prevProducts) => {
+      const updatedProducts = [...prevProducts];
+      updatedProducts.splice(index, 1);
+      return updatedProducts;
+    });
+    calculateTotalSaleRemove(precio); 
   };
 
   const handleSubmit = async (e) => {
@@ -33,7 +54,14 @@ const Venta = () => {
     console.log("ABRRRRRRRRRRRRR");
 
     try {
-      const success = await VentaRegister(ventaNumber,productList,totalSale,purchaseDate, seller, client);
+      const success = await VentaRegister(
+        ventaNumber,
+        productsChecked,
+        totalSale,
+        purchaseDate,
+        seller,
+        client
+      );
 
       if (success) {
         router.push("/");
@@ -48,177 +76,182 @@ const Venta = () => {
   const fetchData = async () => {
     try {
       const data = await getAllProducts();
-      console.log('Data:', data); 
+      console.log("Data:", data);
       setProductList(data);
     } catch (error) {
       console.error(`Error fetching data: ${error.message}`);
     }
   };
-  
+
   useEffect(() => {
     fetchData();
   }, []);
 
   useEffect(() => {
-    console.log(usuario)
+    console.log(usuario);
   }, [usuario]);
-  
+
   const handleProductAdd = () => {
     openModal();
   };
-
   return (
     <>
     {usuario && usuario.nroVendedor !== null && (
-    <main
-      className="h-screen flex items-center justify-center bg-cover"
-      style={{
-        backgroundImage:
-          "url('https://img.freepik.com/vector-premium/fondo-abstracto-azul-linea-luz-verde-espacio-blanco_156943-56.jpg')",
-      }}
-    >
-      <div className="container mx-auto my-8 p-8 bg-gray-100 shadow-md rounded-md">
-        <h2 className="text-3xl font-bold mb-6">Venta Details</h2>
+      <main
+        className="h-screen flex items-center justify-center bg-cover"
+        style={{
+          backgroundImage:
+            "url('https://img.freepik.com/vector-premium/fondo-abstracto-azul-linea-luz-verde-espacio-blanco_156943-56.jpg')",
+        }}
+      >
+        <div className="container mx-auto my-8 p-8 bg-gray-100 shadow-md rounded-md">
+          <h2 className="text-3xl font-bold mb-6">Venta Details</h2>
 
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">
-            Venta Number
-          </label>
-          <input
-            type="text"
-            className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
-            value={ventaNumber}
-            onChange={(e) => setVentaNumber(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">
-            Client
-          </label>
-          <input
-            type="text"
-            className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
-            value={client}
-            onChange={(e) => setClient(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">
-            Seller
-          </label>
-          <input
-            type="text"
-            className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
-            value={usuario.nroVendedor}
-            readOnly
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">
-            Purchase Date
-          </label>
-          <input
-            type="text"
-            className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
-            value={purchaseDate}
-            onChange={(e) => setPurchaseDate(e.target.value)}
-          />
-        </div>
-
-       <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-600">
-                Product List
-              </label>
-              <ul className="list-disc list-inside">
-                <button
-                  type="button"
-                  onClick={handleProductAdd}
-                  className="text-blue-500"
-                >
-                  Add Product
-                </button>
-              </ul>
-            </div>
-
-            <div className="mb-4">
-  <label className="block text-sm font-medium text-gray-600">
-    Products Selected
-  </label>
-  <ul className="list-inside">
-    {productsChecked.map((product, index) => (
-      <li key={index} className="mb-2">
-        <div className="flex items-center">
-          <div className="bg-blue-500 text-white px-4 py-2 rounded-md mr-4">
-            {product.nombre}
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Venta Number
+            </label>
+            <input
+              type="text"
+              className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
+              value={ventaNumber}
+              onChange={(e) => setVentaNumber(e.target.value)}
+            />
           </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Client
+            </label>
+            <input
+              type="text"
+              className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
+              value={client}
+              onChange={(e) => setClient(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Seller
+            </label>
+            <input
+              type="text"
+              className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
+              value={usuario.nroVendedor}
+              readOnly
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Purchase Date
+            </label>
+            <input
+              type="text"
+              className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
+              value={purchaseDate}
+              onChange={(e) => setPurchaseDate(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Product List
+            </label>
+            <ul className="list-disc list-inside">
+              <button
+                type="button"
+                onClick={handleProductAdd}
+                className="text-blue-500"
+              >
+                Add Product
+              </button>
+            </ul>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Products Selected
+            </label>
+            <ul className="list-inside">
+              {productsChecked.map((product, index) => (
+                <li key={index} className="mb-2">
+                  <div className="flex items-center">
+                    <div className="bg-blue-500 text-white px-4 py-2 rounded-md mr-4">
+                      {product.nombre}
+                    </div>
+                    <button
+                      onClick={() => handleDeleteProductSelect(index , product.precio)}
+                      className="bg-red-500 text-white px-4 py-2 rounded-md"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Total Sale
+            </label>
+            <input
+              type="text"
+              className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
+              value={totalSale}
+              readOnly
+            />
+          </div>
+
           <button
-            onClick={() => handleDeleteProductSelect(product)}
-            className="bg-red-500 text-white px-4 py-2 rounded-md"
+            onClick={handleSubmit}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
           >
-            Remove
+            Submit
           </button>
         </div>
-      </li>
-    ))}
-  </ul>
-</div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">
-            Total Sale
-          </label>
-          <input
-            type="text"
-            className="form-input mt-1 block w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:border-blue-500"
-            value={totalSale}
-            onChange={(e) => setTotalSale(e.target.value)}
-          />
-        </div>
 
-        <button
-          onClick={handleSubmit}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Product List Modal"
         >
-          Submit
-        </button>
-      </div>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Product List Modal"
-      >
-        <h2 className="text-2xl font-bold mb-4">Product List</h2>
-        <div className="flex flex-wrap justify-center">
-  {productList.map((product, index) => (
-    <div
-      key={product.id}
-      className="flex-shrink-0 bg-white p-4 rounded-md shadow-md flex flex-col items-center m-2"
-      style={{ width: "150px" }}
-    >
-      <img
-        src={product.imageUrl}
-        alt={product.nombre}
-        className="mb-2 rounded-md"
-        style={{ width: "100%", height: "100px", objectFit: "cover" }}
-      />
-      <p className="text-lg font-semibold">{product.nombre}</p>
-      <button
-        onClick={() => handleProductSelect(product)}
-        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md"
-      >
-        Add
-      </button>
-    </div>
-  ))}
-</div>
-        <button onClick={closeModal} className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">
-          Close
-        </button>
-      </Modal>
-    </main>)}
+          <h2 className="text-2xl font-bold mb-4">Product List</h2>
+          <div className="flex flex-wrap justify-center">
+            {productList.map((product, index) => (
+              <div
+                key={product.id}
+                className="flex-shrink-0 bg-white p-4 rounded-md shadow-md flex flex-col items-center m-2"
+                style={{ width: "150px" }}
+              >
+                <img
+                  src={product.imageUrl}
+                  alt={product.nombre}
+                  className="mb-2 rounded-md"
+                  style={{ width: "100%", height: "100px", objectFit: "cover" }}
+                />
+                <p className="text-lg font-semibold">{product.nombre}</p>
+                <button
+                  onClick={() => handleProductSelect(product)}
+                  className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md"
+                >
+                  Add
+                </button>
+              </div>
+            ))}
+          </div>
+          <button
+            onClick={closeModal}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+          >
+            Close
+          </button>
+        </Modal>
+      </main>
+    )}
     {usuario == null && (
   <div className="alert-message">
     INGRESE SESIÓN POR FAVOR, AGUSTÍN
